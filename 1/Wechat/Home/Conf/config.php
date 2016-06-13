b6065b1beaeef9fdebda2cf253294f528f74d2f5
张翔 <zhangxiang@zhangxiang.local>
2016-06-13 14:50:09 +0800

    1

--- 1/Wechat/Home/Conf/config.php
+++ 1/Wechat/Home/Conf/config.php
@@ -1,20 +1,20 @@
 <?php
 return array(
     //数据库连接参数
-//    'DB_TYPE'   =>'mysql',
-//    'DB_HOST'   =>'127.0.0.1',
-//    'DB_USER'   =>'root',
-//    'DB_PWD'    =>'123456',
-//    'DB_NAME'   =>'jmdc',
-//    'DB_PREFIX' =>'zhx_',
+   'DB_TYPE'   =>'mysql',
+   'DB_HOST'   =>'127.0.0.1',
+   'DB_USER'   =>'root',
+   'DB_PWD'    =>'812010166',
+   'DB_NAME'   =>'jmdc',
+   'DB_PREFIX' =>'zhx_',
 
 	//'配置项'=>'配置值'
-    'SAE_MYSQL_USER'  =>'nxoxjlmnok',
-    'SAE_MYSQL_PASS'  =>'mzxlm15j12jy5k3zj52k23ljj2j2k0wk2yz25x2j',
-    'SAE_MYSQL_HOST_M'=>'zhx_zhxjmdc',
-    'SAE_MYSQL_HOST_S'=>'',
-    'SAE_MYSQL_PORT'  =>'3307',
-    'SAE_MYSQL_DB'    =>'Mysql',
+    // 'SAE_MYSQL_USER'  =>'nxoxjlmnok',
+    // 'SAE_MYSQL_PASS'  =>'mzxlm15j12jy5k3zj52k23ljj2j2k0wk2yz25x2j',
+    // 'SAE_MYSQL_HOST_M'=>'zhx_zhxjmdc',
+    // 'SAE_MYSQL_HOST_S'=>'',
+    // 'SAE_MYSQL_PORT'  =>'3307',
+    // 'SAE_MYSQL_DB'    =>'Mysql',
 
     'TOKEN' => 'zhxjmdc',                           //微信接入token
     'APPID' => 'wx3baa722d40c17edf',                //appID
